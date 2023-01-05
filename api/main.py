7d7e3afab691f166352369a12de624ca52231f3c
cjespanola <cjespanolacj@gmail.com>
2023-01-05 22:50:31 +0800

    docker for api and frontend

--- api/main.py
+++ api/main.py
@@ -5,35 +5,33 @@ from dotenv import load_dotenv
 from flask_cors import CORS
 
 
-load_dotenv(dotenv_path=('./.env.local'))
+load_dotenv(dotenv_path=("./.env.local"))
 
-UNSPLASH_URL="https://api.unsplash.com/photos/random"
-UNSPLASH_KEY=os.environ.get('UNSPLASH_KEY', '')
-DEBUG=bool(os.environ.get('DEBUG', True))
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 
 if not UNSPLASH_KEY:
-  raise EnvironmentError('UNSPLASH_KEY environment variable not set')
+    raise EnvironmentError("UNSPLASH_KEY environment variable not set")
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['DEBUG'] = DEBUG
+app.config["DEBUG"] = DEBUG
 
-@app.route('/new-image')
+
+@app.route("/new-image")
 def new_image():
-    word = request.args.get('query')
-    headers = {
-      "Accept-Version": "v1",
-      "Authorization": "Client-ID " + UNSPLASH_KEY
-      }
+    word = request.args.get("query")
+    headers = {"Accept-Version": "v1",
+               "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
 
-    response = requests.get(url = UNSPLASH_URL, headers = headers, params=params)
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
 
 
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5050)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
